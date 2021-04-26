654c818e92a3b982be07bb4718848086ee2b13c7
Oleh Debretseni <o.debretseni@tenantcloud.com>
2021-04-26 21:07:19 +0300

    modify: test credential in workflow v9

--- tagger.sh
+++ tagger.sh
@@ -6,13 +6,21 @@ cd "${GITHUB_WORKSPACE}"/"${source}" || exit
 
 tcctl message --text "Start tagger"
 
-tcctl run git config --global user.email "o.debretseni@tenantcloud.com"
-tcctl run git config --global user.name "Oleh Debretseni"
-
 NEW_GIT_TAG=$(tcctl helpers generate_increment_version)
 
 tcctl message --text "The new tag is: $NEW_GIT_TAG"
 
-tcctl helpers push_new_version --tag "$NEW_GIT_TAG"
+remote=$(git config --get remote.origin.url)
+repo=$(basename "$remote" .git)
+commit=$(git rev-parse HEAD)
+
+curl -s -X POST https://api.github.com/repos/"$REPO_OWNER"/"$repo"/git/refs \
+-H "Authorization: token $GITHUB_TOKEN" \
+-d @- << EOF
+{
+  "ref": "refs/tags/$NEW_GIT_TAG",
+  "sha": "$commit"
+}
+EOF
 
 tcctl message --text "Finish"
