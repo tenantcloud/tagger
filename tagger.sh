ddc6e07b6382fcf54d789d7715c63437e155cf24
Oleh Debretseni <o.debretseni@tenantcloud.com>
2021-04-30 15:35:43 +0300

    modify: test credential in workflow v13

--- tagger.sh
+++ tagger.sh
@@ -2,10 +2,6 @@
 
 tcctl message --text "Start tagger"
 
-tcctl run git config --global user.name "tenantcloudteam"
-
-tcctl run git config --global user.email team@tenantcloud.com
-
 NEW_GIT_TAG=$(tcctl helpers generate_increment_version)
 
 tcctl message --text "The new tag is: $NEW_GIT_TAG"
